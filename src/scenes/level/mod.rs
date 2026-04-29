c8bce500931a11339b87208dd0a72fc16cfc46f4
Samneet Singh <samsing@yelp.com>
2026-04-29 12:23:59 -0400

    Extract setup_level and spawn helpers into level/systems.rs
    Level mod.rs now only contains LevelPlugin registration. Setup logic
    and player spawning functions live in level/systems.rs, consistent
    with the components.rs + systems.rs pattern used by other modules.

--- src/scenes/level/mod.rs
+++ src/scenes/level/mod.rs
@@ -1,16 +1,16 @@
+mod systems;
+
 use bevy::prelude::*;
 use bevy_ggrs::prelude::*;
 
-use crate::core::config::TILE_SIZE;
 use crate::core::states::GameState;
-use crate::net::{GameMode, is_networked};
+use crate::net::is_networked;
 
-use super::bullet::components::*;
+use super::bullet::components::{Bullet, FireCooldown};
 use super::bullet::systems::*;
-use super::map::systems::{tile_position, load_level, spawn_tiles};
-use super::player::components::{LocalPlayer, NetworkPlayer};
 use super::player::systems::*;
-use super::tank::components::*;
+use super::tank::components::TankAnimation;
+use systems::setup_level;
 
 pub struct LevelPlugin;
 
@@ -45,112 +45,4 @@ impl Plugin for LevelPlugin {
             .rollback_component_with_clone::<Bullet>()
             .rollback_component_with_clone::<FireCooldown>();
     }
-}
-
-// --- Setup ---
-
-fn setup_level(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    game_mode: Res<GameMode>,
-) {
-    commands.spawn(Camera2d);
-
-    let level_data = load_level(1);
-    let grid = level_data.to_tile_grid();
-    spawn_tiles(&mut commands, &asset_server, &grid);
-
-    match *game_mode {
-        GameMode::Local => {
-            spawn_local_player(
-                &mut commands,
-                &asset_server,
-                8,
-                24,
-                KeyCode::ArrowUp,
-                KeyCode::ArrowDown,
-                KeyCode::ArrowLeft,
-                KeyCode::ArrowRight,
-                "sprites/tanks/player1/level1",
-            );
-        }
-        GameMode::OnlineHost(_) | GameMode::OnlineJoin(_) => {
-            let local_handle: usize = if matches!(*game_mode, GameMode::OnlineHost(_)) {
-                0
-            } else {
-                1
-            };
-
-            spawn_network_player(&mut commands, &asset_server, 0, 8, 24, "sprites/tanks/player1/level1");
-            spawn_network_player(&mut commands, &asset_server, 1, 16, 24, "sprites/tanks/player2/level1");
-
-            commands.insert_resource(bevy_ggrs::LocalPlayers(vec![local_handle]));
-        }
-    }
-}
-
-fn spawn_local_player(
-    commands: &mut Commands,
-    asset_server: &Res<AssetServer>,
-    col: i32,
-    row: i32,
-    up: KeyCode,
-    down: KeyCode,
-    left: KeyCode,
-    right: KeyCode,
-    sprite_path: &'static str,
-) {
-    let pos = tile_position(col, row);
-    commands.spawn((
-        Sprite {
-            image: asset_server.load(format!("{sprite_path}/up_f0.png")),
-            custom_size: Some(Vec2::splat(TILE_SIZE)),
-            ..default()
-        },
-        Transform::from_translation(pos),
-        TankAnimation {
-            timer: Timer::from_seconds(0.15, TimerMode::Repeating),
-            frame: 0,
-            direction: Direction::Up,
-        },
-        LocalPlayer {
-            up,
-            down,
-            left,
-            right,
-            sprite_path,
-        },
-        Tank,
-        FireCooldown::new(),
-    ));
-}
-
-fn spawn_network_player(
-    commands: &mut Commands,
-    asset_server: &Res<AssetServer>,
-    handle: usize,
-    col: i32,
-    row: i32,
-    sprite_path: &'static str,
-) {
-    let pos = tile_position(col, row);
-    commands.spawn((
-        Sprite {
-            image: asset_server.load(format!("{sprite_path}/up_f0.png")),
-            custom_size: Some(Vec2::splat(TILE_SIZE)),
-            ..default()
-        },
-        Transform::from_translation(pos),
-        TankAnimation {
-            timer: Timer::from_seconds(0.15, TimerMode::Repeating),
-            frame: 0,
-            direction: Direction::Up,
-        },
-        NetworkPlayer {
-            handle,
-            sprite_path,
-        },
-        Tank,
-        FireCooldown::new(),
-    ));
 }
